9f01268c4b7c60b0e8597d9298b169b398bb1612
weizishun <weizishun108@hellobike.com>
2021-11-26 14:38:04 +0800

    update

--- src/mobi/zishun/sort/QuickSort.java
+++ src/mobi/zishun/sort/QuickSort.java
@@ -13,6 +13,8 @@ public class QuickSort {
             return;
         }
         int q = partition(nums, p, r);
+        // 注意！！是q-1不是q!
+        // 第一次分区后-q即时中间的元素了
         quickSortRecursion(nums, p, q - 1);
         quickSortRecursion(nums, q + 1, r);
     }
@@ -20,22 +22,20 @@ public class QuickSort {
     // 分区，选定q，原地移动nums中元素
     private static int partition(int[] nums, int p, int q) {
         int pivot = nums[q];
-        //游标i对应的是什么数字并不关心，它只是一个分界线，游标i前面的区间都是小于poit的
+        //游标i对应的是什么数字并不关心，它只是一个分界线，游标i前面的区间都是小于pivot的
         int i = p;
         for (int j = p; j < q; j++) {
             if (nums[j] < pivot) {
-                if (i == j) {
-                    ++i;
-                } else {
+                if (i != j) {
                     int temp = nums[i];
-                    nums[i++] = nums[j];
+                    nums[i] = nums[j];
                     nums[j] = temp;
                 }
+                i++;
             }
         }
-        int temp2 = nums[q];
         nums[q] = nums[i];
-        nums[i] = temp2;
+        nums[i] = pivot;
         return i;
     }
 
