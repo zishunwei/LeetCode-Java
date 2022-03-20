2fffd07d3f2a64d4dd49a69c0d1363ec4adeb6f2
weizishun <weizishun108@hellobike.com>
2022-03-20 18:35:58 +0800

    update backtracking related

--- src/mobi/zishun/backtracking/Combinations.java
+++ src/mobi/zishun/backtracking/Combinations.java
@@ -24,15 +24,33 @@ import java.util.List;
 public class Combinations {
     private final List<List<Integer>> result = new ArrayList<>();
 
-    private final List<Integer> temp = new ArrayList<>();
+    private final List<Integer> path = new ArrayList<>();
 
     public List<List<Integer>> combine(int n, int k) {
+        // 从 1 开始是题目的设定
         dfs(1, n, k);
         return result;
     }
 
-    private void dfs(int i, int n, int k) {
-
+    private void dfs(int start, int n, int k) {
+        // 剪枝
+        if (path.size() + (n - start + 1) < k) {
+            return;
+        }
+        // 递归终止条件是：path 的长度等于 k
+        if (path.size() == k) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        // 遍历可能的搜索起点
+        for (int i = start; i <= n; i++) {
+            // 向路径变量里添加一个数
+            path.add(i);
+            // 下一轮搜索，设置的搜索起点要加 1，因为组合数理不允许出现重复的元素
+            dfs(i + 1, n, k);
+            // 重点理解这里：深度优先遍历有回头的过程，因此递归之前做了什么，递归之后需要做相同操作的逆向操作
+            path.remove(path.size() - 1);
+        }
     }
 
 
