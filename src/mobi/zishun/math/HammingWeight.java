ca98681e8bb843d20ad3c0f222e007cd655fdf64
weizishun <weizishun108@hellobike.com>
2022-03-22 17:18:33 +0800

    update math related

---
+++ src/mobi/zishun/math/HammingWeight.java
@@ -0,0 +1,24 @@
+package mobi.zishun.math;
+
+/*
+ * 191. 位1的个数
+编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为汉明重量）。
+提示：
+请注意，在某些语言（如 Java）中，没有无符号整数类型。在这种情况下，输入和输出都将被指定为有符号整数类型，并且不应影响您的实现，因为无论整数是有符号的还是无符号的，其内部的二进制表示形式都是相同的。
+在 Java 中，编译器使用二进制补码记法来表示有符号整数。因此，在上面的 示例 3 中，输入表示有符号整数 -3。
+输入：00000000000000000000000000001011
+输出：3
+解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。
+* https://leetcode-cn.com/problems/number-of-1-bits/
+ */
+public class HammingWeight {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int result = 0;
+        while (n != 0){
+            n = n & (n-1);
+            result++;
+        }
+        return result;
+    }
+}
