17fb2b7e9f596d4f8cecafe71d2b8d9d7bf663f7
weizishun <weizishun108@hellobike.com>
2022-03-28 20:22:08 +0800

    update tree related

--- src/mobi/zishun/binarytree/ConstructBinaryTree.java
+++ src/mobi/zishun/binarytree/ConstructBinaryTree.java
@@ -2,6 +2,9 @@ package mobi.zishun.binarytree;
 
 import mobi.zishun.model.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * 105. 从前序与中序遍历序列构造二叉树
 给定两个整数数组 preorder 和 inorder ，
@@ -23,9 +26,42 @@ inorder 保证 为二叉树的中序遍历序列
 * https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
  */
 public class ConstructBinaryTree {
+
+    // 存储中序遍历的节点位置
+    private final Map<Integer, Integer> hashMap = new HashMap<>();
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        TreeNode result = new TreeNode(preorder[0]);
-        return result;
+        for (int i = 0; i < inorder.length; i++) {
+            hashMap.put(inorder[i], i);
+        }
+        return recursion(preorder, 0, preorder.length, inorder, 0, inorder.length);
+    }
+
+    // inorder 和 inEnd可去掉
+    private TreeNode recursion(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd) {
+        // preorder 为空，直接返回 null
+        if (preStart == preEnd) {
+            return null;
+        }
+        int rootVal = preorder[preStart];
+        TreeNode root = new TreeNode(rootVal);
+        // 在中序遍历中找到根节点的位置
+        int inRootIndex = hashMap.get(rootVal);
+        // 中序遍历中找到根节点的位置左边即为左子树的节点，右边即为右子数的节点
+        int leftNum = inRootIndex - inStart; // 左子树节点的树目
+        // 递归的构造左子树
+        root.left = recursion(preorder, preStart + 1, preStart + 1 + leftNum, inorder, inStart, inRootIndex);
+        // 递归的构造右子树
+        root.right = recursion(preorder, preStart + 1 + leftNum, preEnd, inorder, inRootIndex + 1, inEnd);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        ConstructBinaryTree m = new ConstructBinaryTree();
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        TreeNode result = m.buildTree(preorder, inorder);
+        System.out.println(result);
     }
 
 }
