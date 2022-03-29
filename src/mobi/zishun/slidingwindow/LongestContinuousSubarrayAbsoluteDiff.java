360bbc7bf4fe3503d9308015105dfacb97da6fd1
weizishun <weizishun108@hellobike.com>
2022-03-29 19:53:42 +0800

    update sw related

--- src/mobi/zishun/slidingwindow/LongestContinuousSubarrayAbsoluteDiff.java
+++ src/mobi/zishun/slidingwindow/LongestContinuousSubarrayAbsoluteDiff.java
@@ -1,5 +1,7 @@
 package mobi.zishun.slidingwindow;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.TreeMap;
 
 /*
@@ -55,6 +57,40 @@ public class LongestContinuousSubarrayAbsoluteDiff {
             right++;
         }
         return res;
+    }
+
+    // 仅需要统计当前窗口内的最大值与最小值，因此我们也可以分别使用两个单调队列解决本题。
+    // 在实际代码中，我们使用一个单调递增的队列 queMin 维护最小值，一个单调递减的队列 queMax 维护最大值。
+    // 这样我们只需要计算两个队列的队首的差值，即可知道当前窗口是否满足条件。
+    public int longestSubarrayV2(int[] nums, int limit) {
+        Deque<Integer> queMax = new LinkedList<Integer>();
+        Deque<Integer> queMin = new LinkedList<Integer>();
+        int n = nums.length;
+        int left = 0;
+        int right = 0;
+        int ret = 0;
+        while (right < n) {
+            while (!queMax.isEmpty() && queMax.peekLast() < nums[right]) {
+                queMax.pollLast();
+            }
+            while (!queMin.isEmpty() && queMin.peekLast() > nums[right]) {
+                queMin.pollLast();
+            }
+            queMax.offerLast(nums[right]);
+            queMin.offerLast(nums[right]);
+            while (!queMax.isEmpty() && !queMin.isEmpty() && queMax.peekFirst() - queMin.peekFirst() > limit) {
+                if (nums[left] == queMin.peekFirst()) {
+                    queMin.pollFirst();
+                }
+                if (nums[left] == queMax.peekFirst()) {
+                    queMax.pollFirst();
+                }
+                left++;
+            }
+            ret = Math.max(ret, right - left + 1);
+            right++;
+        }
+        return ret;
     }
 
 }
