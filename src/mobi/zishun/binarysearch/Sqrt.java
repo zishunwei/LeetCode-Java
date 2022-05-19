10424b9a20cc3aadb767fa490a54b51987e7e1ba
weizishun <weizishun108@hellobike.com>
2022-05-19 14:55:24 +0800

    update bs related

--- src/mobi/zishun/binarysearch/Sqrt.java
+++ src/mobi/zishun/binarysearch/Sqrt.java
@@ -3,42 +3,33 @@ package mobi.zishun.binarysearch;
 /*
 69. Sqrt(x)
 给你一个非负整数 x ，计算并返回x的 算术平方根 。
-
 由于返回类型是整数，结果只保留 整数部分 ，小数部分将被 舍去 。
-
+提示：
+0 <= x <= 2^31 - 1
 链接：https://leetcode-cn.com/problems/sqrtx
  */
 public class Sqrt {
     public int mySqrt(int x) {
-        if (x == 0) {
-            return 0;
-        }
-        long left = 1;
-        long right = x;
+        int left = 0;
+        int right = x;
+        int res = -1;
         while (left <= right) {
-            long medium = (right - left) / 2 + left;
-            if (medium * medium > x) {
-                right = medium - 1;
-            } else if (medium * medium == x) {
-                return (int) medium;
+            int medium = (right - left) / 2 + left;
+            long product = (long) medium * medium;
+            if (product <= x) {
+                // 结果是向下取整
+                // 当product <= x时即可能触及答案
+                res = medium;
+                left = medium + 1;
             } else {
-//            else if (medium * medium < x) {
-                if ((medium + 1) * (medium + 1) > x) {
-                    return (int) medium;
-                } else
-                if ((medium + 1) * (medium + 1) == x) {
-                    return (int) medium + 1;
-//              else if ((medium + 1) * (medium + 1) < x) {
-                } else {
-                    left = medium + 1;
-                }
+                right = medium - 1;
             }
         }
-        return -1;
+        return res;
     }
 
     public static void main(String[] args) {
         Sqrt m = new Sqrt();
-        System.out.println(m.mySqrt(2147395599));
+        System.out.println(m.mySqrt(1));
     }
 }
